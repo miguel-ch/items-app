a2105a44559d436ae1c481675de00b375808e9b7
Miguel Ch <miguelch0808@gmail.com>
2023-02-21 10:17:58 -0500

    Header logo fix

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import box from "../../assets/box.svg";
 
 const AppHeader = () => {
   return (
     <header>
       <div className="flex flex-row items-center gap-x-3 w-full p-4 bg-slate-900">
-        <img src="/box.svg" className="h-12" alt="Logo" />
+        <img src={box} className="h-12" alt="Logo" />
         <p className="text-4xl font-bold text-white opacity-60">Items list</p>
       </div>
     </header>
